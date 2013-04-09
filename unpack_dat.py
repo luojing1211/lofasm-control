69f30d6d8966789e1738ad27ad718837941d0fde
Louis P. Dartez <louis.dartez@gmail.com>
2013-04-09 18:10:48 -0500

    more comments added to unpack_dat.py

--- unpack_dat.py
+++ unpack_dat.py
@@ -84,9 +84,10 @@ class lofasm_packet:
 #################################################################3
 #Begin class:lofasm_word
 
-class lofasm_word:
+class lofasm_word:                                          #class to facilitate handling of a single 8byte word
     def __init__(self,word=0):
-        if word==0:
+        if word==0:                                         #if there is no data to process make everything zero
+                                                                #this mode is not yet used in the current implementation
             self.stream_id = 0
             self.hdr_version = 0
             self.ack_num = 0
@@ -95,95 +96,140 @@ class lofasm_word:
             self.dsamp3 = 0
             self.dsamp4 = 0
         else:
-            stream_id,hdr_ver,ack_cnt,data = parseWord(word)
-            self.stream_id = stream_id
+            stream_id,hdr_ver,ack_cnt,data = parseWord(word)    #parse raw word and extract encapsulated info
+            self.stream_id = stream_id                          #set metadata
             self.hdr_version = hdr_ver
             self.ack_num = ack_cnt
-            self.dsamp1 = data[0]
+            self.dsamp1 = data[0]                               #make sampled data part of the object
             self.dsamp2 = data[1]
             self.dsamp3 = data[2]
             self.dsamp4 = data[3]           
 
-    def get_data(self):
+    def get_data(self):                                         #interface methods for lofasm_word class
         return[self.dsamp1,self.dsamp2,self.dsamp3,self.dsamp4]
+
     def get_ack_num(self):
         return self.ack_num
+
     def get_hdr_version(self):
         return self.hdr_version
+
     def get_stream_id(self):
         return self.stream_id
 # end of class: lofasm_word
-#####################################################################33
-def toggle(bit):
+#####################################################################
+#begin toggle
+def toggle(bit):                                            #toggle a single bit
     if bit:
         return 0
     else:
         return 1
+#endof: toggle
+
+##################################################################
+#begin num2bit
 def num2bit(word,bit_len=16):
-    bit_check=[]
+    '''Convert integer number to bitmap array. \nUsage: num2bit(int[,bit_len=16])
+    This function returns a list (<list>).'''
+    
+    bit_check=[]                            #populates an array containing the value each bit reperesents
+                                                #the MSB (LHS) represents 2^(N-1), where N is the number of bits.
+                                                #if bit_len == 3 then bit_check == [4,2,1].
     for i in range(bit_len):
         bit_check.append(2**(bit_len-i-1))
     
-    bit_stat = []
-    for bit in bit_check:
-        val = bit & word
+    bit_stat = []                           #bitmap array to store the status of each bit.
+    
+    #begin for loop
+    for bit in bit_check:                      
+        val = bit & word                    #use AND to determine whether or not corresponding bit should be set
         if val:
-            bit_stat.append(1)
+            bit_stat.append(1)              #append bit status as element in bitmap array
         else: bit_stat.append(0)
-    return bit_stat
-          
+    #end for loop
+
+    return bit_stat                         #type: <list>
+#endof num2bit
+###################################################################
+#begin bit2num
 def bit2num(word,bit_len=16,twos_comp = 1):
+    '''
+    Convert a bitmap to an integer number. Two's Complement is assumed by default.
+    Usage: bit2num(int[,bit_len=16[,twos_comp =1]])
+    Returns: signed int
+    '''
     num=0
-    if not twos_comp:    
-        for i in range(bit_len):
+    if not twos_comp:                                   #if not in two's complement form then our job is easy   
+        #begin forloop
+        for i in range(bit_len):                        #iterate through bitmap and assign each bit its value and add
             if word[i]:
                 num += 2**(bit_len - (i+1))
-        return num 
-    else:
-        sign = word[0]
-        if sign==0:
-            return bit2num(word[1:],bit_len=bit_len-1,twos_comp=0)
-        word=word[1:] #get rid of the sign bit
-        togg_stop = None
-        togg_set = 0
-        bit_len -= 1
-        #index of -1 is equal to index of bit_len-1
-        i=0
-        while not togg_set:
-            #print "i",i
-            sys.stdout.flush()
-            
-            j=-1*(i+1)
-            #print "j",j
-            #print 'word[j]',word[j]
-            if word[j]: 
-                togg_stop = bit_len + j
-                togg_set = 1
-            i+=1
+        #end forloop
+
+        return num                                      #type <int>
+    else:                                               #if in two's completement... work a bit harder
         
-        if togg_stop == 0:
-            return -1*bit2num(word,bit_len = len(word),twos_comp=0)
+        sign = word[0]                                  #retrieve sign bit
+        
+        
+        if sign==0:                                     #if sign is unset then our job is easy again :)
+            return bit2num(word[1:],bit_len=bit_len-1,twos_comp=0)
+
+
+        word=word[1:]                                   #get rid of the sign bit
+        bit_len -= 1                                    #bit_len decreases by one since we removed the sign bit
+        
+        togg_stop = None                                #stopping index for toggle sweep
+        togg_set = 0                                    #var to determine whether a toggle stop point has been set
+        
+        i=0                                             #counter used to iterate word
+        
+        #begin whileloop
+        while not togg_set:
+            
+            j=-1*(i+1)                                  #set index to LSB (right) and traverse towards MSB (left)
+            
+            
+            if word[j]:                                 #if bit is set then record current index in togg_stop
+                togg_stop = bit_len + j                 # convert to positive index
+                togg_set = 1                            #toggle stop point has been set
+            i+=1                                        #increment counter
+        #end whileloop
+
+        if togg_stop == 0:                              #then return 0
+            #return -1*bit2num(word,bit_len = len(word),twos_comp=0)
+            return 0
         else:
-            for i in range(togg_stop):
-                word[i] = toggle(word[i])
+            for i in range(togg_stop):                  #iterate word up to the toggle stop point
+                word[i] = toggle(word[i])               #flip bit
             return -1*bit2num(word,bit_len = len(word),twos_comp=0)
-                
+#end bit2num
+###################################################################################  
+#begin parseWord
+
 def parseWord(word):
+    '''
+    Parse and extract LoFASM data from an 8 byte (64bit) binary word.
+    Usage: parseWord(word)
+    '''
+    #header
     #bit map
     #0: stream id
     #1-3: hdr_ver
     #4-7: ack_cnt
     #8-63: data
-    word_num = np.array(struct.unpack('>Q',word)) 
-    word_bitmap = num2bit(word_num,bit_len=64)
-    hdr_bitmap = word_bitmap[:8]
-    data_bitmap = word_bitmap[8:]
+    word_num = np.array(struct.unpack('>Q',word))               #unpack data as unsigned long long (one large number)
+    word_bitmap = num2bit(word_num,bit_len=64)                  #generate bitmap for this word
+    hdr_bitmap = word_bitmap[:8]                                #extract header info from first byte
+    data_bitmap = word_bitmap[8:]                               #everything else is data (56 bits, 4*14bits)
     
-    stream_id = hdr_bitmap[0]
-    hdr_ver_bitmap = hdr_bitmap[1:4]
-    ack_cnt_bitmap = hdr_bitmap[4:]
-    snap_bitmap = []
-    snap_bitmap.append(data_bitmap[:14])
+    #header population
+    stream_id = hdr_bitmap[0]                                   #stream (I&Q) id assignment
+    hdr_ver_bitmap = hdr_bitmap[1:4]                            #header version from ROACH
+    ack_cnt_bitmap = hdr_bitmap[4:]                             #the ack number occupies the last 4 bits of the header
+
+    snap_bitmap = []                                            #extract all four data samples (voltage snapshots)
+    snap_bitmap.append(data_bitmap[:14])                            #and append to an array in order
     snap_bitmap.append(data_bitmap[14:28])
     snap_bitmap.append(data_bitmap[28:42])
     snap_bitmap.append(data_bitmap[42:])
@@ -192,19 +238,27 @@ def parseWord(word):
     #print hdr_ver_bitmap
     #print ack_cnt_bitmap
     #print len(snap_bitmap[0])
-    hdr_ver = bit2num(hdr_ver_bitmap,bit_len=3,twos_comp=0)
+    hdr_ver = bit2num(hdr_ver_bitmap,bit_len=3,twos_comp=0)     #convert metadata from bitmaps to real numbers
     ack_cnt = bit2num(ack_cnt_bitmap,bit_len=4,twos_comp=0)
-    data=[[]]*4
+    
+    
+    data=[[]]*4                                                 #to hold sampled data; i forgot why i didn't just use
+                                                                # data=[0]*4 since bit2num returns int
     
     for i in range(4):
         data[i] = bit2num(snap_bitmap[i],bit_len=14)
     return stream_id,hdr_ver,ack_cnt,data
+
+#end parseWord
 ####################################################
+#begin gen_next_ack
 def gen_next_ack(curr_ack,lo_ack=0,hi_ack=15): 
     if curr_ack >= hi_ack:
         return lo_ack
     else:
         return curr_ack + 1
+#end gen_next_ack
+#####################################################
 def gen_prev_ack(curr_ack,lo_ack=0,hi_ack=15):
     if curr_ack == lo_ack:
         return hi_ack
